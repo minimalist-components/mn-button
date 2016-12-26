a948bb48ad6e348ebccb5dea6dcec8ea8666d00e
Darlan Mendonça <darlanmendonca@gmail.com>
2016-12-26 16:40:04 -0200

    add vendor dependencies

---
+++ tasks/vendorCSS.js
@@ -0,0 +1,25 @@
+import gulp from 'gulp'
+import bowerFiles from 'bower-files'
+import minifyCss from 'gulp-minify-css'
+import concat from 'gulp-concat'
+
+gulp.task('vendorCSS', vendorCSSTask)
+
+function vendorCSSTask() {
+  let dependencies = bowerFiles()
+    .ext('css')
+    .files
+
+  const devDependencies = bowerFiles()
+    .ext('css')
+    .dev()
+    .files
+
+  dependencies = dependencies.concat(devDependencies)
+
+  return gulp
+    .src(dependencies)
+    .pipe(concat('vendor.css'))
+    .pipe(minifyCss({keepSpecialComments: 0}))
+    .pipe(gulp.dest('./docs'))
+}
