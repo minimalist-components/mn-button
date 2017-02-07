f30bff4528bee364885d00398691a97e8f3309aa
Darlan Mendonça <darlanmendonca@gmail.com>
2017-02-07 19:38:18 -0200

    proposal to default type in buttons inside a form

--- sources/scripts/mn-button.js
+++ sources/scripts/mn-button.js
@@ -1,4 +1,13 @@
 document.addEventListener('click', blur)
+const buttons = document.querySelectorAll('button.mn-button')
+
+Array
+  .from(buttons)
+  .forEach(button => {
+    if (button.closest('form') && !button.getAttribute('type')) {
+      button.setAttribute('type', 'button')
+    }
+  })
 
 function blur(event) {
   const isMnButton = event.target.classList.contains('mn-button')
